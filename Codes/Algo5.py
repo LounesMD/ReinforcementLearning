7ada8441e2a078eba267ff0b94cd1528abcedc0b
Lounes MD <84915434+LounesMD@users.noreply.github.com>
2021-06-28 15:10:15 +0200

    ALgorithme SARSA

--- Codes/Algo5.py
+++ Codes/Algo5.py
@@ -2,7 +2,9 @@
 """
 Created on Wed Jun 23 09:28:28 2021
 
-@author: Lounès
+@author: Lounès Meddahi
+
+Fichier avec une implémentation de l'algorithme SARSA appliquée à un labyrinthe
 """
 
 
@@ -13,238 +15,42 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set_theme()
 import math
 
-
-#####################################Fonctions utiles#####################################
-def conversion(c):
-    """
-    Fonction qui permet de convertir le caractère c en un certain nombre
-    """
-    if(c=='*'):
-        return -100
-    elif(c ==' '):
-        return 0
-    elif(c=='O'):
-        return -1
-    elif(c=='V'):
-        return 100
-
-
-
-file = open("Maze.txt","r")
-
-maze = list()
-for line in file:
-    l = list()
-    for character in line:
-        if(character != '\n'):
-            l.append(conversion(character))
-    maze.append(l)
-    
-maze.reverse() #Ce la va nous permettre d'avoir le (0,0) en bas à gauche de notre labyrinthe
+from Utile import *
 
 
 
 
-def etats_labyrinthe():
-    """
-    On va définir tous les états possibles du labyrinthe. Ce qui correspond aux coordonnées du plateau
-    """
-    l = list()
-    for i in range(16):
-        for j in range(16):
-            l.append([i,j])
-    return l
-
-def actions_labyrinthe():
-    """
-    Cette fonction va nous permettre d'avoir la liste des actions possibles pour le jeu du labyrinthe.
-    On va supposer que:
-        0 -> aller à droite
-        1 -> aller en haut
-        2 -> aller à droite
-        3 -> aller en bas
-    Returns
-    -------
-    list
-        actions.
-    """
-    return [0,1,2,3]
-
-def exectution_labyrinthe(etat,action):
-    """
-    fonction qui va permettre d'executer l'action passer en paramètre dans l'environnement du labyrinthe et renvoyer:
-        (retour , etat) qui sont les choses à observer une fois l'action effectuée
-    """
-    p=etat.copy()
-    if(action==0):
-        if(p[1]+1 > len(maze[0]) - 1):
-            return ( maze[p[0]][p[1]] , p )
-        p[1]+=1
-        return ( maze[p[0]][p[1]] , p )
-    if(action==1):
-        if(p[0]+1 > len(maze) -1 ):   
-            return ( maze[p[0]][p[1]] , p )
-        p[0]+=1
-        return ( maze[p[0]][p[1]] , p )
-    if(action==2):
-        if(p[1]-1 < 0):
-            return ( maze[p[0]][p[1]] , p )
-        p[1]-=1
-        return ( maze[p[0]][p[1]] , p )
-    if(action==3):
-        if(p[0]-1 < 0):
-            return ( maze[p[0]][p[1]] , p )
-        p[0]-=1
-        return ( maze[p[0]][p[1]] , p )
-
-
-def init_labyrinthe():
-    """
-    Point de départ du labyrinthe
-    """
-    return [1,1]
-
-def est_final_labyrinthe(s):
-    """
-    Permet de savoir si on est arrivé sur la cellule finale
-    """
-    return s == [10,11]
-
-def decision_labyrinthe():
-    """
-    """
-    a = choice(actions_labyrinthe())
-    return a
-
-
-
-
-def boltzmann(Q , actions , s , to):  
-    """
-    Fonction qui va choisir une action à faire en utilisant le modèle de Boltzmann
-
+def Sarsa(etats , actions , gamma , environnement , init , decision , execution , final ):       
+    """    
     Parameters
     ----------
-    Q : list
-        Fonction qualitée.
-    actions : list
-        Actions possibles.
-    s : list
-        coordonnées.
-    to : int
-        température .
-
-    Returns
-    -------
-    l : list
-        (Renvoie une distribution de probabilité des actions en suivant le modele de Boltzmann).
-
-    """
-    l =  list()
-    for action in actions:
-        p = 0     
-        ex = math.exp((Q[s[1]][s[0]][action])/to)        
-        v = 0        
-        for a in actions:     
-            v+= math.exp((Q[s[1]][s[0]][a])/to)        
-        p = ex / v
-        l.append(p)
-    return l
-    
-
-def gloutonne(Q , actions , s , a):
-    """
-    Fonction pour le modèle glouton
-
-    Parameters
-    ----------
-    Q : TYPE
+    etats : TYPE
         DESCRIPTION.
     actions : TYPE
         DESCRIPTION.
-    s : TYPE
+    gamma : TYPE
+        DESCRIPTION.
+    environnement : TYPE
+        DESCRIPTION.
+    init : TYPE
+        DESCRIPTION.
+    decision : TYPE
+        DESCRIPTION.
+    execution : TYPE
+        DESCRIPTION.
+    final : TYPE
         DESCRIPTION.
 
     Returns
     -------
-    TYPE
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    Q : TYPE
         DESCRIPTION.
 
     """
-    l = list()
-    for action in actions:
-        l.append( ( Q[s[1]][s[0]][action] ,action) )
-    
-    p = list(v[0] for v in l )
-    
-    for elt in p:
-        if(elt != 0):
-            return max(l , key=lambda x:x[0] )[1]
-    return choice(actions)
-
-CSI="\x1B["
-def affichage2_0(maze , s , final):    
-    ligne = len(maze)
-    colonne =  len(maze[0])
-    for i in range(ligne):
-        for j in range(colonne):
-            if(i == s[0] and j ==s[1]):
-                print (CSI+"30;40m" + " A" + CSI + "0m" , end='')
-                
-            #elif(final[0]==i and final[1]==j):
-             #  print(CSI+"34;44m" + " S" + CSI + "0m" , end='')
-                
-            elif(maze[i][j] == -100 ):                
-                print(CSI+"31;41m" +' W' + CSI + "0m" , end='')
-            elif(maze[i][j]==0):
-                print(CSI+"37;47m" + " P" + CSI + "0m" , end='')
-            
-            elif(maze[i][j]==100):
-               print(CSI+"34;44m" + " S" + CSI + "0m" , end='')
-
-            else:
-                print(CSI+"32;42m" + " D" + CSI + "0m",end='')
-        print('')
-        
-        
-        
-def Eps_Gloutonne( Q , act ,  etat , epsilon ):
-    """
-    Fonction epsilon-gloutonne 
-
-    Parameters
-    ----------
-    Q : list
-        fonction qualité courante.
-    act : list
-        listes des actions possibles.
-    epsilon : int
-        entier epsilon.
-    etat : list
-        coordonée de l'état courant.
-
-    Returns
-    -------
-    int
-        renvoie l'action à faire en ayant fait un choix epsilon-glouton.
-    """
-    v = choices( [1,2] , weights = [1-epsilon , epsilon] , k=1 ) #Cela va nous permettre d'avoir soit un choix glouton, soit aléatoire
-
-    if v ==[1]:        
-        li = list()
-        for action in act:
-            li.append((Q[etat[1]][etat[0]][action] , action))
-
-        max_val= max(li , key = lambda x: x[0])
-        return max_val[1]
-
-    else: 
-        return choice(act)
-##########################################################################################
-
-
-
-def Sarsa(etats , actions , gamma , environnement , init , decision , execution , final ):        
     #Q va nous permettre de stocker la qualité pour tous les couples (etat , action) sous forme de matrice
     eps = 50
     
@@ -338,7 +144,7 @@ def Sarsa(etats , actions , gamma , environnement , init , decision , execution 
             N[s[t][1]][s[t][0]][a[t]] += 1
             t+=1
             
-            print(t , cpt)
+            #print(t , cpt)
             #print(s[t])
             #affichage2_0(maze , s[t] , [10,11])
         eps = eps/2  
